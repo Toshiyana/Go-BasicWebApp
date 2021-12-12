fa4a775ef3bc82ac4570eae475c1d66c935664b2
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-12-12 17:13:01 +0900

    refactor: Use packages

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,18 +1,20 @@
-package main
+package handlers
 
 import (
 	"net/http"
+
+	"myapp/pkg/render"
 )
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	renderTamplate(w, "home.page.tmpl")
+	render.RenderTamplate(w, "home.page.tmpl")
 	// fmt.Fprintf(w, "This is the home page")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	renderTamplate(w, "about.page.tmpl")
+	render.RenderTamplate(w, "about.page.tmpl")
 
 	// sum := addValues(2, 2)
 	// _, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,4 +1,4 @@
-package main
+package render
 
 import (
 	"fmt"
@@ -6,7 +6,8 @@ import (
 	"net/http"
 )
 
-func renderTamplate(w http.ResponseWriter, tmpl string) {
+// RenderTemplate renders templates using html/template
+func RenderTamplate(w http.ResponseWriter, tmpl string) {
 	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
