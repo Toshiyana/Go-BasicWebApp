39e2a8a7f4dfae66c9fc141c0616b74d4e71665f
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-12-12 16:55:17 +0900

    refactor: use bootstrap in tmplates

---
+++ handlers.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"net/http"
+)
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTamplate(w, "home.page.tmpl")
+	// fmt.Fprintf(w, "This is the home page")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	renderTamplate(w, "about.page.tmpl")
+
+	// sum := addValues(2, 2)
+	// _, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+}
+
+// func addValues(x, y int) int {
+// 	return x + y
+// }
+
+// // Divide is the divide page handler
+// func Divide(w http.ResponseWriter, r *http.Request) {
+// 	dividend := 100.0
+// 	divisor := 0.0
+// 	f, err := divideValues(dividend, divisor)
+// 	if err != nil {
+// 		fmt.Fprintf(w, "cannot divide by zero")
+// 		return
+// 	}
+
+// 	fmt.Fprintf(w, fmt.Sprintf("%f divide by %f is %f", dividend, divisor, f))
+// }
+
+// func divideValues(x, y float64) (float64, error) {
+// 	if y == 0 {
+// 		err := errors.New("cannot divide by zero")
+// 		return 0, err
+// 	}
+// 	result := x / y
+// 	return result, nil
+// }
--- main.go
+++ main.go
@@ -2,61 +2,11 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 )
 
 // declare portNumber out of main() because I never want the portNumber to be changed by another part of the application.
 const portNumber = ":8080"
-
-// Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTamplate(w, "home.page.tmpl")
-	// fmt.Fprintf(w, "This is the home page")
-}
-
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTamplate(w, "about.page.tmpl")
-
-	// sum := addValues(2, 2)
-	// _, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
-}
-
-// func addValues(x, y int) int {
-// 	return x + y
-// }
-
-// // Divide is the divide page handler
-// func Divide(w http.ResponseWriter, r *http.Request) {
-// 	dividend := 100.0
-// 	divisor := 0.0
-// 	f, err := divideValues(dividend, divisor)
-// 	if err != nil {
-// 		fmt.Fprintf(w, "cannot divide by zero")
-// 		return
-// 	}
-
-// 	fmt.Fprintf(w, fmt.Sprintf("%f divide by %f is %f", dividend, divisor, f))
-// }
-
-// func divideValues(x, y float64) (float64, error) {
-// 	if y == 0 {
-// 		err := errors.New("cannot divide by zero")
-// 		return 0, err
-// 	}
-// 	result := x / y
-// 	return result, nil
-// }
-
-func renderTamplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("Error parsing template:", err)
-		return
-	}
-}
 
 // main is the main application function
 func main() {
