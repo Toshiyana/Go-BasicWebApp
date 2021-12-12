c50dec5c93333c0a68981e8d578c709630df5874
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-12-12 15:06:40 +0900

    feat: Add page handler

--- main.go
+++ main.go
@@ -5,21 +5,34 @@ import (
 	"net/http"
 )
 
+// declare portNumber out of main() because I never want the portNumber to be changed by another part of the application.
+const portNumber = ":8080"
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+}
+
+// addValues adds two integers and return the sum
+func addValues(x, y int) int {
+	return x + y
+}
+
+// main is the main application function
 func main() {
-	// r: request type use pointer
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Don't use Println() because it print in console
-		n, err := fmt.Fprintf(w, "Hello world")
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		fmt.Printf("Number of bytes written: %d\n", n)
-	})
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 
 	// start web server that listens for requests
 	// listen to particular port of my computer (In this case, listen on 8080)
-	_ = http.ListenAndServe(":8080", nil)
+	_ = http.ListenAndServe(portNumber, nil)
 	// access localhost:8080 in browser
 }
